41d0973031e8b6e7b916902d460b32353d9faec7
[deakacoli] <[dk61403@ubt-uni.net]>
2023-06-14 09:53:30 +0200

    foto e fshire

--- src/component/Login/LoginForm.js
+++ src/component/Login/LoginForm.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 //import "./Login.css";
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
-import logo from "./logo.png"
+
 import { decodeToken } from './jwtUtils';
 
 
--- src/component/Login/Registration.js
+++ src/component/Login/Registration.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 
-import logo from "./logo.png";
+
 
 function Registration() {
   
