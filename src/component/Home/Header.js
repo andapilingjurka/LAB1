5aaf0a2a69604da24f34d5829cecda0d05b76e55
ArdiBerdyna <Ardi_881@hotmail.com>
2023-06-14 12:02:16 +0200

    header

--- src/component/Home/Header.js
+++ src/component/Home/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import './Header.css';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = ()=>{
     return(
@@ -9,7 +9,8 @@ const Header = ()=>{
                     <h2>Bringing loving care to health care</h2>
                     <p>In our pharmacies, you will be assisted by our qualified staff and you will have easy and quick access to the medicines and medical tools you need.</p>
                     <p>Rush to our pharmacies for a healthier and happier life!</p>
-                    <button style={{backgroundColor:"#024585e5"}}>Order Now </button>
+                    <Link to="/Products/" style={{color:'white', textDecoration:'none'}} ><button style={{backgroundColor:"#024585e5"}}> Order Now</button></Link> 
+
                     <button style={{backgroundColor:"#024585e5"}}>Learn More</button>
                 </div>
                 <div>
